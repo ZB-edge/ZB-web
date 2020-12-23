1a7e0a9d21ae85c1726c9a7ae242ed244642bcd7
fornate <1692359975@qq.com>
2020-12-23 21:00:21 +0800

    change port

--- src/network/request_image.js
+++ src/network/request_image.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Global from '@/components/common/global'
 export default function request_image(config){
   const instance1 = axios.create({
-    baseURL: 'http://'+ Global.ip +':8100/api/damage/',
+    baseURL: 'http://'+ Global.ip +':8105/api/damage/',
     timeout: 5000,
   })
   return instance1(config)
